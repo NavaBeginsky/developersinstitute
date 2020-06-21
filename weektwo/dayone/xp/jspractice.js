8ce3d09222c8e5ace28ee1878827e748399fd696
Nava <navag93@gmail.com>
2020-06-21 15:29:21 +0300

    xp finished

--- weektwo/dayone/xp/jspractice.js
+++ weektwo/dayone/xp/jspractice.js
@@ -3,6 +3,14 @@ console.log('Hi There! It\'s ' + '"sunny" outside');
 
 
 // exercise 3 (first two were only in the console for the most part)
-let string1 = "Here is my string the includes the word Nemo. Now I need to find it."
-nemoIndex = string1.indexOf('Nemo');
-console.log()
+let string1 = "Here is my string, the word Nemo is included. Now I need to find it."
+let array1 = string1.split(' ');
+let nemoPosition = array1.indexOf('Nemo');
+
+if(nemoPosition != -1) {
+	console.log(`I found Nemo at ${nemoPosition}`);
+}
+
+else {
+	console.log("I can't find nemo :(");
+}
