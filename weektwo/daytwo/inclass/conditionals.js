8492f1a3858e40cbcf85e2e9093a8b3476ce9b41
Nava <navag93@gmail.com>
2020-06-22 11:15:04 +0300

    exercises 2 and 3

--- weektwo/daytwo/inclass/conditionals.js
+++ weektwo/daytwo/inclass/conditionals.js
@@ -9,3 +9,41 @@ if (age < 18) {
 } else {
 	alert("Please enter a valid age");
 }
+
+
+//exercise 2 Write as comments the steps of the resolution of this piece of code
+let a = 2 + 2;
+
+switch (a) { //checking value of a against upcoming values
+  case 3: //if a ==3, display alert
+    alert( 'Too small' );
+    break;
+  case 4: // if a ==4, display the following alert  This is the correct answer
+    alert( 'Exactly!' );
+    break;
+  case 5: // if a == 5 display the following alert 
+    alert( 'Too large' );
+    break;
+  default: //if a was none of the previous values, display the following alert
+    alert( "I don't know such values" );
+}
+
+
+//exercise 3 Write as comments the steps of the resolution of this piece of code
+
+let a = 2 + 2;
+
+switch (a) {//checking value of a against upcoming values
+  case 4: //if a == 4, display the following alert
+    alert('Right!');
+    break;
+
+  case 3: // (*) grouped two cases, if a==3 or a==5 it will display the two following alerts
+  case 5:
+    alert('Wrong!');
+    alert("Why don't you take a math class?");
+    break;
+
+  default: //if it was none of the previous values, it will display the following alert.
+    alert('The result is strange. Really.');
+}
