86f7d0c09d1f99a152fadf3de922759fb95cdd70
Nava <navag93@gmail.com>
2020-06-22 19:57:31 +0300

    responsive form

--- weektwo/daytwo/xpninja/xpninja.js
+++ weektwo/daytwo/xpninja/xpninja.js
@@ -1,7 +1,21 @@
-let birth1;
-let birth2;
+// let birth1; //older
+// let birth2;//younger
 
-let ageDiff = birth2 - birth1;
+function getFormData() {
+	let birth1 = document.getElementById('date1').value;
+	let birth2 = document.getElementById('date2').value;
+	let ageDiff = birth2 - birth1;
+	console.log(ageDiff);
+	document.getElementById('results').innerHTML = "birth2 + ageDiff";
+	
+}
+
+// function getHalfAge(birth1, birth2) {
+// 	let ageDiff = birth2 - birth1;
+// 	document.getElementById('results').innerHTML = birth2 + ageDiff;
+// 	return birth2 + ageDiff;
+// }
+
 
 
 
