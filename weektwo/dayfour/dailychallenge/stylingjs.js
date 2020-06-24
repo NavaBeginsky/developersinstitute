0337ba8f98f9b7efd6c6dd22c3d4fe7f5230c74f
Nava <navag93@gmail.com>
2020-06-24 16:35:35 +0300

    daily challenge

--- weektwo/dayfour/dailychallenge/stylingjs.js
+++ weektwo/dayfour/dailychallenge/stylingjs.js
@@ -1,7 +1,10 @@
-let stringOfWords = prompt('Give me a string of words');
-let arrayOfWords = stringOfWords.split(" ");
-console.log('*'.repeat(stringOfWords.length));
-for(let word of arrayOfWords){
-	console.log(`* ${word}` + " ".repeat(stringOfWords.length - word.length - 3) + "*");
+
+function putABorderOnIt(){
+	let stringOfWords = prompt('Give me a string of words');
+	let arrayOfWords = stringOfWords.split(" ");
+	console.log('*'.repeat(stringOfWords.length));
+	for(let word of arrayOfWords){
+		console.log(`* ${word}` + " ".repeat(stringOfWords.length - word.length - 3) + "*");
+	}
+	console.log('*'.repeat(stringOfWords.length));
 }
-console.log('*'.repeat(stringOfWords.length));
