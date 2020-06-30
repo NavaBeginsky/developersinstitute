6c68870519176e4f393dd97ec5be392c0a5002fc
Nava <navag93@gmail.com>
2020-06-30 13:57:04 +0300

    daily exercises

--- weekthree/daytwo/inclass/inclass.js
+++ weekthree/daytwo/inclass/inclass.js
@@ -9,3 +9,27 @@ function shopping(list, currency, amountOfMoney){
 }
 
 shopping(shoppingList, '$', 10);
+
+
+function insert_Row(){
+    let row = document.createElement('tr');
+    let table = document.getElementById('sampleTable').children[0];
+    let i = 0;
+    while(i < 2){
+        let column = document.createElement('td');
+        column.innerHTML = `column ${i}`;
+        row.appendChild(column);
+        i++
+    }
+    table.appendChild(row);
+}
+
+let button = document.getElementById('jsstyle');
+button.addEventListener('click', changeColor);
+button.addEventListener('mouseover', changeText);
+function changeColor(e){
+    e.target.style.backgroundColor = 'blue'
+}
+function changeText(e){
+    e.target.innerHTML = 'hover';
+}
