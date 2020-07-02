f4f7755001b0f458642be23e3e7abbdd5d41a61c
Nava <navag93@gmail.com>
2020-07-02 16:24:48 +0300

    level 3

--- weekthree/hackathon/learninggame.js
+++ weekthree/hackathon/learninggame.js
@@ -1,5 +1,6 @@
 let imageSection = document.getElementById('imageSection');
 let numberOfImages;
+let numberOfImages2;
 let numbersOnPage;
 let resetNumber;
 let instructionSection = document.getElementById('instructions');
@@ -9,6 +10,10 @@ let numberSection = document.getElementById('numberSection');
 let audioSection = document.getElementById('myAudio');
 let currentLevel = 1;
 let levelUpNumbers = document.querySelector('#numberSection .levelup');
+let total;
+let column1 = document.querySelector('#column1');
+let column2 = document.querySelector('#column2');
+let column3 = document.querySelector('#column3');
 
 function chooseRandomImage(){
     let images = [
@@ -51,7 +56,6 @@ function displayImages(){
     } else {
         numberOfImages = Math.ceil(Math.random() * 10);
     }
-    let imageSection = document.getElementById('imageSection');
     let i = 0;
     while(i < numberOfImages){
         let imageDisplay = document.createElement('img');
@@ -60,6 +64,51 @@ function displayImages(){
         i++
     }
     return image;
+}
+
+function resetLevelThreeImages(){
+    column1.innerHTML = '';
+    column2.innerHTML = '';
+    column3.innerHTML = '';
+}
+
+function displayImagesLevelThree(){
+    let image = chooseRandomImage();
+    numberOfImages = Math.ceil(Math.random() * 5);
+    numberOfImages2 = Math.ceil(Math.random() * 5);
+    let numberOfImageArray = [];
+    while(numberOfImages == numberOfImages2){
+        numberOfImages2 = Math.ceil(Math.random() * 5);
+    }
+    if(numberOfImages > numberOfImages2){
+        numberOfImageArray[0] = numberOfImages;
+        numberOfImageArray[1] = numberOfImages2;
+    } else {
+        numberOfImageArray[0] = numberOfImages2;
+        numberOfImageArray[1] = numberOfImages1;
+    }
+    resetLevelThreeImages();
+    let i = 0;
+    while(i < numberOfImageArray[0]){
+        let imageDisplay = document.createElement('img');
+        imageDisplay.setAttribute('src', image['url']);
+        column1.appendChild(imageDisplay);
+        i++
+    }
+    let operators = ['images/plus.jpg', 'images/minus.png'];
+    let chooseOperator = Math.floor(Math.random() * 2);
+    let chosenOperator = operators[chooseOperator];
+    let operatorDisplay = document.createElement('img');
+    operatorDisplay.setAttribute('src', chosenOperator);
+    column2.appendChild(operatorDisplay);
+    i = 0;
+    while(i < numberOfImageArray[1]){
+        let imageDisplay = document.createElement('img');
+        imageDisplay.setAttribute('src', image['url']);
+        column3.appendChild(imageDisplay);
+        i++
+    }
+    total = chooseOperator == 0 ? numberOfImageArray[0] + numberOfImageArray[1] : numberOfImageArray[0] - numberOfImageArray[1];
 }
 
 function getNumberChosen(numberLocation){
@@ -79,17 +128,23 @@ function turnNumberIdToNum(numString){
 }
 
 function checkIfCorrectNumberChosen(){
+    console.log('hereIam');
     let numChosen = getNumberChosen(event.target);
+    console.log('here');
     if(typeof resetNumber == 'object'){
         resetNumber.style.color = 'black';
     }
-    if(numChosen == numberOfImages){
+   if(numChosen == numberOfImages || currentLevel === 3 && total == numChosen){
         imageSection.innerHTML = '';
         result = markCorrect();
         if (result != "stop") {playGame()};
 
     } else {
         event.target.style.color = 'red';
+        event.target.classList.add('shakeNumber');
+        event.target.addEventListener("animationend", () => {
+            event.target.classList.remove("shakeNumber");
+        });
         markIncorrect();
     }
     resetNumber = event.target;
@@ -140,10 +195,17 @@ function playGame(){
     if(playButton != null){
         playButton.remove();
     }    
-    let image = displayImages();
-    instructionSection.innerHTML = `How many ${image['name']} do you see?`;
-    playAudio(image);
-    trackNumberClickandValidate();
+    if(currentLevel === 3){
+        displayImagesLevelThree();
+        instructionSection.innerHTML = `It's time to do some math!`;
+        trackNumberClickandValidate();
+    }
+    else {
+        let image = displayImages();
+        instructionSection.innerHTML = `How many ${image['name']} do you see?`;
+        playAudio(image);
+        trackNumberClickandValidate();
+    }
 }
 
 function gameOver(){
@@ -153,9 +215,10 @@ function gameOver(){
 
 function youWin(){
     confetti.start(3000);
-    if(currentLevel === 1){
+    if(currentLevel < 3){
         levelUp();
     } else{
+        resetLevelThreeImages();
         instructionSection.innerHTML = "You win! Want to play again?";
         restartButton();
     }
