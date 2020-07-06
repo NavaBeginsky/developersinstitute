ab45bf0e556960060d85a1d7191ce7fb96f1dab9
Nava <navag93@gmail.com>
2020-07-06 16:07:50 +0300

    exercises finished

---
+++ weekfour/dayone/xp/xp.py
@@ -0,0 +1,30 @@
+#exercise 1
+print('hello world\n' * 4)
+
+#exercise 2
+print((99 ** 3) * 8)
+
+#exercise 3
+name = 'nava'
+age = 27
+shoe_size = 10
+info = f'My name is {name} and I\'m {age} years old. I have size {shoe_size} feet!'
+print(info)
+
+#exercise 4
+computer_brand = "ASUS"
+print(f'I have an {computer_brand} computer')
+
+#exercise 6
+height = input('what is your height in inches?')
+if int(height) >= 35 :
+    print('You are tall enough')
+else :
+    print('You are not tall enough')
+
+#exercise 7
+number = input('Give me a number')
+if int(number) % 2 == 0 :
+    print('The number is even')
+else : 
+    print('The number is odd')
---
+++ weekfour/daytwo/inclass/inclass.py
@@ -0,0 +1,19 @@
+list1 = [5, 10, 15, 20, 25, 50, 20]
+index_of_20 = list1.index(20)
+if  index_of_20 > -1:
+    list1[index_of_20] = 200
+print(list1)
+
+
+#unpack
+aTuple = (10, 20, 30, 40)
+one, two, three, four = aTuple
+
+
+n = int(input('give me a number'))
+multiplication_table = []
+for num in range(1,11):
+    multiplication_table.append(num * n)
+
+print(multiplication_table)
+    
