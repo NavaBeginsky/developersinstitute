199951b73fe077e285f904b7abaf6d29cff46a25
Nava <navag93@gmail.com>
2020-07-14 13:50:23 +0300

    xp finished

--- weekfive/daytwo/xp.py
+++ weekfive/daytwo/xp.py
@@ -17,22 +17,23 @@ class Family():
                 return person['age'] >= 18
 
     def __repr__(self):
-        return self.members
+        family_members = 'The members of the family are:'
+        for member in self.members:
+            family_members += f'\n{member["name"]}'
+        return family_members
 
-    def get_family(self):
-        print(self.members)
-        print(repr(self.members))
 
-
-beginsky = Family('Beginsky', [{'name':'Michael','age':35,'gender':'Male','is_child':False}, {'name':'Sarah','age':32,'gender':'Female','is_child':False}])
-beginsky.born(name='Maya', gender = 'Female')
+beginsky = Family('Beginsky', [{'name': 'Michael', 'age': 35, 'gender': 'Male', 'is_child': False}, {
+                  'name': 'Sarah', 'age': 32, 'gender': 'Female', 'is_child': False}])
+beginsky.born(name='Maya', gender='Female')
 print(beginsky.is_18('Michael'))
-beginsky.__repr__()
+print(beginsky)
 
 
 class TheIncredibles(Family):
     '''**members info to be passed to super class should be a list containing dictionaries that contain
      name, age, gender, is_child, power, and incredible_name'''
+
     def use_power(self, name):
         for person in self.members:
             if person['name'] == name:
@@ -40,20 +41,27 @@ class TheIncredibles(Family):
                     print(person['power'])
                 else:
                     raise Exception('You have no power here!')
-    
+
     def incredible_presentation(self):
         for person in self.members:
-            print(f"{person['name']} your incredible name is {person['incredible_name']} and your incredible power is {person['power']}")
-           
+            print(
+                f"{person['name']} your incredible name is {person['incredible_name']} and your incredible power is {person['power']}")
+
 
 incredibles = TheIncredibles('Parr', [
-    {'name': 'Bob', 'age': 40, 'gender': 'Male', 'is_child': False, 'power': 'super stregnth', 'incredible_name': 'Mr. Incredible'}, 
-    {'name': 'Helen', 'age': 36, 'gender': 'Female', 'is_child': False, 'power': 'mutating body', 'incredible_name': 'Elastigirl'}, 
-    {'name': 'Violet', 'age': 15, 'gender': 'Female', 'is_child': True, 'power': 'invisibility and force fields', 'incredible_name': 'Invisigirl'}, 
-    {'name': 'Dash', 'age': 10, 'gender': 'Male', 'is_child': True, 'power': 'super speed', 'incredible_name': 'speedy'} 
-    ])
+    {'name': 'Bob', 'age': 40, 'gender': 'Male', 'is_child': False,
+        'power': 'super stregnth', 'incredible_name': 'Mr. Incredible'},
+    {'name': 'Helen', 'age': 36, 'gender': 'Female', 'is_child': False,
+        'power': 'mutating body', 'incredible_name': 'Elastigirl'},
+    {'name': 'Violet', 'age': 15, 'gender': 'Female', 'is_child': True,
+        'power': 'invisibility and force fields', 'incredible_name': 'Invisigirl'},
+    {'name': 'Dash', 'age': 10, 'gender': 'Male', 'is_child': True,
+        'power': 'super speed', 'incredible_name': 'speedy'}
+])
 
 incredibles.incredible_presentation()
 
-incredibles.born(name = 'Baby Jack', gender = 'Male', power = 'Unknown', incredible_name = 'SuperJack')
+incredibles.born(name='Baby Jack', gender='Male',
+                 power='Unknown', incredible_name='SuperJack')
+print(incredibles)
 incredibles.incredible_presentation()
