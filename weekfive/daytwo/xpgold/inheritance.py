86871ac073f6e2ee83c432f27abe499a7e055e6a
Nava <navag93@gmail.com>
2020-07-15 11:33:28 +0300

    bank account confusion complete

--- weekfive/daytwo/xpgold/inheritance.py
+++ weekfive/daytwo/xpgold/inheritance.py
@@ -35,14 +35,13 @@ my_pets.walk()
 
 #exercise 2
 class Bank():
-    def __init__(self):
-        self.num_clients = 0
+    num_clients = 0
 
 class BankAccount(Bank):
     def __init__(self, owner, balance):
         if self.num_clients < 10:
             self.owner = owner
-            self.balance = balance
+            self.balance = int(balance)
             self.num_clients += 1
         else :
             print('The bank is already at max capacity.')
@@ -51,8 +50,10 @@ class BankAccount(Bank):
         if amount > 0:
             self.balance += amount
             print('deposit successful')
+            print(self.balance)
         else :
             print('That is not a valid deposit amount')
+            print(self.balance)
     
     def withdraw(self, amount):
         if amount > 0 and amount <= self.balance:
@@ -63,24 +64,66 @@ class BankAccount(Bank):
         else :
             print('Please enter a valid amount to withdraw')
 
-    class Owner(BankAccount):
-        def __init__(self, owner, balance, cc_num, password):
-            super().__init__(owner, balance)
-            self.cc_num = cc_num
-            self.password = password
-        
-        def check_owner_info(self):
-            user_cc = input('What is your credit card number?')
-            user_pw = input('What is your password?')
-            while user_cc != self.cc_num :
-                user_cc = input('That was not correct, what is your credit card number?') 
+class Owner(BankAccount):
+    def __init__(self, owner, balance, cc_num, password):
+        super().__init__(owner, balance)
+        self.cc_num = cc_num
+        self.password = password
+    
+    def check_owner_info(self):
+        user_cc = int(input('What is your credit card number?'))
+        user_pw = input('What is your password?')
+        while user_cc != self.cc_num :
+            user_cc = int(input('That was not correct, what is your credit card number?')) 
+        if user_pw != self.password:
+            user_pw = input('Incorrect password, what is your password?')
             if user_pw != self.password:
-                user_pw = input('What is your password?')
-                if user_pw != self.password:
-                    print("Wrong password, the card has been eaten by the machine")
-                    del self.cc_num
-                else:
-                    deposit_or_withdraw = input('Would you like to make a "deposit" or "withraw"?')
-                    # self.deposit() if deposit_or_withdraw == 'deposit' else self.withdraw()
+                print("Wrong password, the card has been eaten by the machine")
+                del self.cc_num
+                return
+        deposit_or_withdraw = input('Would you like to make a "deposit" or "withraw"?')
+        self.deposit() if deposit_or_withdraw == 'deposit' else self.withdraw()
+
+    def deposit(self):
+        bills = {
+            20: 0,
+            50: 0,
+            100: 0, 
+            }
+        while True:    
+            bill = int(input("The machine only accepts bills in the amount of 20, 50 and 100. Which bill would you like to deposit"))
+            quantity = int(input(f'How many {bill} would you like to deposit?'))
+            if bill == 20 or bill == 50 or bill == 100 and quantity > 0:
+                bills[bill] += quantity
+                print('successfully deposited', bill * quantity, 'dollars')
+                more_bills = input('Would you like to deposit more bills?')
+                if more_bills == 'no':
+                    amount = calculate_total(bills)
+                    super().deposit(amount)
+                    break
 
 
+    def withdraw(self):
+        bills = {
+            20: 0,
+            50: 0,
+            100: 0
+        }
+        while True:
+            bill = int(input("The machine only gives 20s and 50s. Which bill would you like to withdraw?"))
+            quantity = int(input('How many would you like?'))
+            if bill == 20 or bill == 50 and quantity > 0:
+                bills[bill] += quantity
+                more_bills = input('Would you like to withdraw more bills?')
+                if more_bills == 'no':
+                    amount = calculate_total(bills)
+                    super().withdraw(amount)
+                    break
+            else :
+                print('please choose a valid bill')
+
+def calculate_total(bills_dict):
+    return bills_dict[20] * 20 + bills_dict[50] * 50 + bills_dict[100] * 100
+
+nava = Owner('nava', '50', 34, 'blah')
+nava.check_owner_info()
