abdba2389d5ef37e361a0c543013a08caf4500f2
NavaBeginsky <navag93@gmail.com>
2020-07-23 15:17:36 +0300

    xp

--- weekfive/dayfour/xp/sentencegenerator.py
+++ weekfive/dayfour/xp/sentencegenerator.py
@@ -1,7 +1,27 @@
-
+from random import choice
 
 def get_words_from_file(my_file):
     with open(my_file, 'r+') as word_file:
-        return map(str.rstrip, word_file.readlines())
+        return [word.rstrip() for word in word_file.readlines()]
 
-print(get_words_from_file('sowpods.txt'))
+def get_random_sentence(sentence_length, word_array):
+    my_sentence = ''
+    for i in range(sentence_length):
+        my_sentence += f'{choice(word_array)} '
+    return my_sentence.lower()
+
+def main():
+    print('We will create a random sentence with a number of words of your choosing')
+    try:
+        sentence_length = int(input('How many words would you like in your sentence? (2-20)'))
+        if 2 <= sentence_length <= 20:
+            words = get_words_from_file('sowpods.txt')
+            generated_sentence = get_random_sentence(sentence_length, words)
+            print(generated_sentence)
+        else:
+            print('Invalid number')
+            return
+    except ValueError:
+        print('The sentence generator only accepts integers')
+
+main()
