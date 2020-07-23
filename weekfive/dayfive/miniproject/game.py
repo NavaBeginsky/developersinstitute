3b4ae7effd8542829043a37305fae7c3b867d303
NavaBeginsky <navag93@gmail.com>
2020-07-23 14:14:37 +0300

    removed gt

--- weekfive/dayfive/miniproject/game.py
+++ weekfive/dayfive/miniproject/game.py
@@ -43,37 +43,3 @@ class Game():
         print(
             f'You selected {player_choice}. The computer played {computer_choice}. You {result}.')
         return result
-
-    # the code below doesn't work, the __gt__ method is not actually functioning when I use the greater sign in the get game result. 
-    # I've got no clue why, or how to use this properly if someone would like to explain. I moved most the relevant code here and 
-    # commented out and did a simpler functioning version of the game above.
-
-
-
-    # def __init__(self):
-    #     self.player_choice = ''
-
-    # def get_game_result(self, computer_choice):
-    #     if self.player_choice == computer_choice:
-    #         return 'draw'
-    #     return 'win' if self.player_choice > computer_choice else 'lose'
-
-    # def __gt__(self, computer_choice):
-
-    #     if self.player_choice == 'rock':
-    #         if computer_choice == 'paper':
-    #             return False
-    #         if computer_choice == 'scissors':
-    #             return True
-
-    #     elif self.player_choice == 'paper':
-    #         if computer_choice == 'scissors':
-    #             return False
-    #         if computer_choice == 'rock':
-    #             return True
-
-    #     elif self.player_choice == 'scissors':
-    #         if computer_choice == 'rock':
-    #             return False
-    #         if computer_choice == 'paper':
-    #             return True
