a2cd266011fdfd39dc8d2831b5eede2c5f901833
Nava <navag93@gmail.com>
2020-08-22 22:44:51 +0300

    add hotel form finally functioning

--- weeknine/finalproject_djangoproject/unique_hotels/contact_forms/views.py
+++ weeknine/finalproject_djangoproject/unique_hotels/contact_forms/views.py
@@ -6,35 +6,36 @@ from hotels import models
 from django.contrib import messages
 from django.forms.models import modelformset_factory
 
+
 # Create your views here.
 def addHotel(request):
     imageFormSet = modelformset_factory(
         models.HotelPhotos, 
         form=HotelPhotosForm, 
-        extra=3
+        extra=5
         )
     if request.method == 'POST':
         hotel_form = AddHotelForm(request.POST)
         photos_formset = imageFormSet(request.POST, request.FILES)
         if hotel_form.is_valid() and photos_formset.is_valid():
             country, created = models.Country.objects.get_or_create(name = hotel_form.cleaned_data['country'])
-            location = models.Location.objects.get_or_create(city=hotel_form.cleaned_data['city'], country=country)
+            location, created = models.Location.objects.get_or_create(city=hotel_form.cleaned_data['city'], country=country)
             hotel = hotel_form.save(commit=False)
             hotel.location = location
             hotel.approved = False
             hotel.save()
-            
-            for form in photos_formset.cleaned_data:
-                models.HotelPhotos(image=form['image'], hotel=hotel).save()
-            
+
+            images = photos_formset.save(commit=False)
+            for image in images:
+                image.hotel = hotel
+                image.save()
+
             for category in hotel_form.cleaned_data['categories']:
-                cat = models.Categories.objects.get(name=category)
-                models.Categories.hotel.add(cat)
+                hotel.categories_set.add(category)
             
             for amenity in hotel_form.cleaned_data['amenities']:
-                amenity = models.Categories.objects.get(name=amenity)
-                models.Amenities.hotel.add(cat)
-            
+                hotel.amenities_set.add(amenity)
+
             messages.success(request, 'Form submission successful')
             
     context = {
