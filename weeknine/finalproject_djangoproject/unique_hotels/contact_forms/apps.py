5e09efd6e5b32efd6b7c29262593005cc30c14fd
Nava <navag93@gmail.com>
2020-08-31 14:39:46 +0300

    moving final project

--- weeknine/finalproject_djangoproject/unique_hotels/contact_forms/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AddHotelsConfig(AppConfig):
-    name = 'add_hotels'
--- weeknine/finalproject_djangoproject/unique_hotels/contact_forms/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('addhotel/', views.addHotel, name='addhotel'),
-    path('contact/', views.Contact.as_view(), name='contact')
-]
